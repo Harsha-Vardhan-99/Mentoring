7810554597b7c1a28e2893c5c46b5fe6b6231e4f
Harsha-Vardhan-9 <f20170546@hyderabad.bits-pilani.ac.in>
2021-07-13 17:45:51 +0530

    changes1

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// function onSubmit(){
+//     console.log(9);
+// //    var name =  document.getElementById("name").value;
+// //    var phone = document.getElementById("phone").value;
+// //    var probdesc = document.getElementById("problemDescription").value;
+// //    var dateandtime = document.getElementById("dateandtime").value;
+// //    document.getElementById("sname").innerHTML = name;
+// //    document.getElementById("sphone").innerHTML = phone;
+// //    document.getElementById("sprobdesc").innerHTML = probdesc;
+// //    document.getElementById("sdateandtime").innerHTML = dateandtime;
+
+// }
+
+$(document).ready(function(){
+
+  });
