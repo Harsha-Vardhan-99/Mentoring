a640eea4c8d43dcc285197a0b72b19dd433f3b8f
Harsha-Vardhan-9 <f20170546@hyderabad.bits-pilani.ac.in>
2021-07-14 21:09:37 +0530

    ValidatorsandModal

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ var gender;
 window.onload=function(){
 
   let form = document.getElementById('myform');
-  
+
   form.addEventListener("submit",(event)=>{
     event.preventDefault();
     name = event.target.name.value;
@@ -17,16 +17,41 @@ window.onload=function(){
     gender = event.target.gender.value;
     probdesc = event.target.problemDescription.value;
     dateandtime = event.target.dateandtime.value;
+    Display();
+    document.getElementById("summarytitle").style.display = "block";
+    document.getElementById("summaryinfo").style.display = "block";
+    window.scrollTo(0,document.body.scrollHeight);
+});
 
-
-    document.getElementById("sname").innerHTML = name;
+function Display(){
+  document.getElementById("sname").innerHTML = name;
     document.getElementById("sphone").innerHTML = phone;
     document.getElementById("sgender").innerHTML = gender;
     document.getElementById("sprobdesc").innerHTML = probdesc;
     document.getElementById("sdateandtime").innerHTML = dateandtime;
-  
-   document.getElementById("summarytitle").style.display = "block";
-   document.getElementById("summaryinfo").style.display = "block";
-});
+   
+}
+
+let reschedule = document.getElementById('reschedule');
+  reschedule.addEventListener("click",()=>{
+    document.getElementById("rname").value = name;
+    document.getElementById("rphone").value = phone;
+    document.getElementById("r"+gender).setAttribute("checked",true);
+    document.getElementById("rproblemDescription").value = probdesc;
+    document.getElementById("rdateandtime").value = dateandtime;
+  });
+
+
+  let update = document.getElementById('myform2');
+  update.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    name = event.target.rname.value;
+    phone = event.target.rphone.value;
+    gender = event.target.rgender.value;
+    probdesc = event.target.rproblemDescription.value;
+    dateandtime = event.target.rdateandtime.value;
+    Display();
+    $("#rescheduleModal").modal("toggle");
+  });
 
 }
