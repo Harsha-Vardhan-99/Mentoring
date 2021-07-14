200bda85c95bfccfee2ef4c8fc25e6426dff0b88
Harsha-Vardhan-9 <f20170546@hyderabad.bits-pilani.ac.in>
2021-07-14 12:31:54 +0530

    ValidatorsandSummary

--- scripts.js
+++ scripts.js
@@ -1,16 +1,32 @@
-// function onSubmit(){
-//     console.log(9);
-// //    var name =  document.getElementById("name").value;
-// //    var phone = document.getElementById("phone").value;
-// //    var probdesc = document.getElementById("problemDescription").value;
-// //    var dateandtime = document.getElementById("dateandtime").value;
-// //    document.getElementById("sname").innerHTML = name;
-// //    document.getElementById("sphone").innerHTML = phone;
-// //    document.getElementById("sprobdesc").innerHTML = probdesc;
-// //    document.getElementById("sdateandtime").innerHTML = dateandtime;
 
-// }
 
-$(document).ready(function(){
+var name;
+var phone;
+var probdesc;
+var dateandtime;
+var gender;
 
-  });
+window.onload=function(){
+
+  let form = document.getElementById('myform');
+  
+  form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    name = event.target.name.value;
+    phone = event.target.phone.value;
+    gender = event.target.gender.value;
+    probdesc = event.target.problemDescription.value;
+    dateandtime = event.target.dateandtime.value;
+
+
+    document.getElementById("sname").innerHTML = name;
+    document.getElementById("sphone").innerHTML = phone;
+    document.getElementById("sgender").innerHTML = gender;
+    document.getElementById("sprobdesc").innerHTML = probdesc;
+    document.getElementById("sdateandtime").innerHTML = dateandtime;
+  
+   document.getElementById("summarytitle").style.display = "block";
+   document.getElementById("summaryinfo").style.display = "block";
+});
+
+}
